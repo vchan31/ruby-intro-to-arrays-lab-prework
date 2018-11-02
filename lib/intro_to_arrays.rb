dbc74070359e94a8527567e7270fddf235a7b0f9
sensible-proc-7557 <vchan313@gmail.com>
2018-11-02 17:41:43 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  return []
+end
+
+def array_with_two_elements
+  return ['Go', 'Fish']
+end
+
+def first_element (array1)
+  return array1[0]
+
+end
+
+def third_element (array1)
+  return array1[2]
+end
+
+def last_element (array1)
+  return array1[-1]
+end
+
+def first_element_with_array_methods (array1)
+  return array1.first
+end
+
+def last_element_with_array_methods (array1)
+  return array1.last
+end
+
+def length_of_array(array1)
+  return array1.length
+end
